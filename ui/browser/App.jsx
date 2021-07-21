2fb6d1cb0b4454f6ec7a259c363ef6f79939bee9
Matt Faucher <51377027+mattfaucher@users.noreply.github.com>
2021-07-21 11:14:36 -0400

    setting up UI environment' '

---
+++ ui/browser/App.jsx
@@ -0,0 +1,18 @@
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+// import Page from '../src/Page.jsx';
+
+const element = (
+	<Router>
+		<Page />
+	</Router>
+);
+
+ReactDOM.hydrate(element, document.getElementById('content'));
+
+if (module.hot) {
+	module.hot.accept();
+}
