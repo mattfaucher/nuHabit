5da33ffc2c226b217e7026f343caf22d485144ad
Matt Faucher <51377027+mattfaucher@users.noreply.github.com>
2021-07-27 20:20:02 -0400

    working on connecting frontend and backed via HabitList

--- api/db.js
+++ api/db.js
@@ -4,7 +4,7 @@ const { MongoClient } = require('mongodb');
 let db;
 
 async function connectToDb() {
-  const url = process.env.DB_URL || 'mongodb://localhost/habits';
+  const url = process.env.DB_URL || 'mongodb://localhost/users';
   const client = new MongoClient(url, { useNewUrlParser: true });
   await client.connect();
   console.log('Connected to MongoDB at', url);
