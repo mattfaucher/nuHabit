15000a4df1f1818f7b2cc4a408193e8a8f9b59f2
Matt Faucher <51377027+mattfaucher@users.noreply.github.com>
2021-07-28 17:00:57 -0400

    working on connecting front and backend

--- ui/src/HabitList.jsx
+++ ui/src/HabitList.jsx
@@ -3,24 +3,41 @@ import {
 	Container,
 } from 'react-bootstrap';
 
+import graphQLFetch from './graphQLFetch.js';
 import Habit from './Habit.jsx';
 import AddHabit from './AddHabit.jsx';
 
 export default class HabitList extends React.Component {
 	static async fetchData(match) {
-		const query = `query user(
-			$email: String
-		){
-			habitsList(
-				title: $title
-				isGood: $isGood
-				increments: $increments
-			)
+		const query = `query ($_id: ID!) {
+			userHabits(_id: $_id) {
+				title increments count isGood created
+			}
 		}`
+		
+		const data = await graphQLFetch(query);
+		return data;
 	}
 
 	constructor(props) {
 		super(props);
+		this.state = {
+			habits,
+		};
+	}
+	
+	componentDidMount() {
+		const { habits } = this.state;
+		if (habits = null) this.loadData();
+	}
+	
+	async loadData() {
+		const data = await HabitList.fetchData(match);
+		if (data) {
+			this.setState({
+				habits: data.habitList,
+			});
+		}
 	}
 
 	render() {
