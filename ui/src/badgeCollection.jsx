0ff64492eb88e6ee1435e8609e4f786ac195f668
Noam <gal.n@husky.neu.edu>
2021-08-04 18:36:42 -0400

    erros with badge collection

--- ui/src/badgeCollection.jsx
+++ ui/src/badgeCollection.jsx
@@ -34,80 +34,26 @@ export default class Collection extends React.Component {
 		});
 	}
 
-	render() {
-        return (
-            <Container fluid="md">
-                <body>
-                    <table class="badgeGrid">
+    getGrid(habbits) {
+        for (let i = 0; i < habbits; i++) {
+            <div class="col">Sample Badge</div>
+        }
+    }
 
-                    </table>
-                </body>
+	render() {
+        const habbits = 5;
+        const badges = 9;
+
+        for (i = 0; i < cars.length; i++) {
+                        
+        }
+        return (
+            
+            <Container fluid="md">
+                <div class="row row-cols-9"></div>
+                    getGrid(habbits);
+
             </Container>
-        )
-		return (
-			<Container fluid="md">
-				<Card border='dark' className='shadow-lg p-3 mb-5 bg-white rounded'>
-					<Card.Body>
-						<Row>
-							<Col className="col-10" align="left">
-								<Form>
-									<Form.Control type="text" placeholder="list of badges"/>
-								</Form>
-							</Col>
-							<Col className="col-2" align="right">
-								<Button variant='success' onClick={this.handleShow}>
-									<FontAwesomeIcon icon={faPlus} />
-								</Button>
-								<Modal show={this.state.showModal} onHide={this.handleClose}>
-									<Modal.Header>Add Habit</Modal.Header>
-									<Modal.Body className="ml-auto">
-										<InputGroup>
-											<InputGroup.Text id="title-input">Title</InputGroup.Text>
-											<FormControl
-												defaultValue={this.state.title}
-												aria-label="Add your new habit"
-												aria-describedby="title-input"
-											/>
-										</InputGroup>
-										<InputGroup>
-											<InputGroup.Text>Form</InputGroup.Text>
-											<InputGroup.Radio
-												aria-label="Form a habit"
-												checked={true === this.state.isGood}
-												onChange={e => { this.setState({ isGood: true }); }}
-											/>
-											<InputGroup.Text>Break</InputGroup.Text>
-											<InputGroup.Radio
-												aria-label="Break a habit"
-												checked={false === this.state.isGood}
-												onChange={e => { this.setState({ isGood: false }); }}
-											/>
-										</InputGroup>
-										<InputGroup>
-											<InputGroup.Text>Daily</InputGroup.Text>
-											<InputGroup.Radio
-												aria-label="Increment Selection"
-												checked={true === this.state.increment}
-												onChange={e => { this.setState({ increment: true }); }}
-											/>
-											<InputGroup.Text>Weekly</InputGroup.Text>
-											<InputGroup.Radio
-												aria-label="Increment Selection"
-												checked={false === this.state.increment}
-												onChange={e => { this.setState({ increment: false }); }}
-											/>
-										</InputGroup>
-									</Modal.Body>
-									<Modal.Footer>
-										<Button variant='secondary' onClick={this.handleClose}>Close</Button>
-										<Button variant='primary' onClick={this.handleSubmit}>Submit</Button>
-									</Modal.Footer>
-								</Modal>
-							</Col>
-						</Row>
-					</Card.Body>
-				</Card>
-			</Container>
-		);
-	}
+            )
+        }
 }
