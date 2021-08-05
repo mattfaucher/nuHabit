1d1c29d32e284867608d05f01365cae0753ede44
cwil290831 <cwil290831@gmail.com>
2021-08-05 10:30:51 -0400

    finished habit details

--- ui/src/HabitList.jsx
+++ ui/src/HabitList.jsx
@@ -1,127 +1,121 @@
-import React from 'react'
-import {
-	Container, Alert,
-} from 'react-bootstrap';
-import { withAuth0, withAuthenticationRequired } from '@auth0/auth0-react';
+import React from "react";
+import { Container, Alert } from "react-bootstrap";
+import { withAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
 
-import graphQLFetch from './graphQLFetch.js';
-import Habit from './Habit.jsx';
-import AddHabit from './AddHabit.jsx';
+import graphQLFetch from "./graphQLFetch.js";
+import Habit from "./Habit.jsx";
+import AddHabit from "./AddHabit.jsx";
 
 class HabitList extends React.Component {
-	async fetchData(email) {
-		// get the user if they exist
-		const user = await this.userExists(email);
-		// if null, create new user
-		if (!user) {
-			const newUser = {
-				name: this.props.auth0.user.name,
-				email: this.props.auth0.user.email,
-			};
-			const vars = { 'user': newUser };
-			const mutation = `mutation ($user: UserInput!) {
+  async fetchData(email) {
+    // get the user if they exist
+    const user = await this.userExists(email);
+    // if null, create new user
+    if (!user.user) {
+      const newUser = {
+        name: this.props.auth0.user.name,
+        email: this.props.auth0.user.email,
+      };
+      const vars = { user: newUser };
+      const mutation = `mutation ($user: UserInput!) {
 				insertUser(user: $user) {
 					name
 					email
-					habitList {
-						title
-					}
 				}
 			}`;
-			const data = await graphQLFetch(mutation, vars);
-			if (!data.habitList) return [];
-			else return data.habitList;
-		} else {
-			// query the habitsList
-			const habits = await this.getHabits(email);
-			return habits;
-		}
-	}
+      const data = await graphQLFetch(mutation, vars);
+      if (!data.habitList) return [];
+      else return data.habitList;
+    } else {
+      // query the habitsList
+      const habits = await this.getHabits(email);
+      return habits;
+    }
+  }
 
-	// Query to find if user exists
-	async userExists(email) {
-		const vars = { 'email': email };
-
-		const query = `query (
-			$email: String!) {
-			userHabits(email: $email) {
-				id title increments count isGood created isDone
-
-		const query = `query ($email: String!) {
-			user (email: $email) {
+  // Query to find if user exists
+  async userExists(email) {
+    const vars = { email: email };
+    const query = `query ($email: String!) {
+			user(email: $email) {
 				name email
-
 			}
 		}`;
-		const data = await graphQLFetch(query, vars);
-		return data;
-	}
+    const data = await graphQLFetch(query, vars);
+    return data;
+  }
 
-	// Query to get the habits for a user
-	async getHabits(email) {
-		const vars = { 'email': email };
-		const query = `query ($email: String!){
-			userHabits(email:$email) {
+  // Query to get the habits for a user
+  async getHabits(email) {
+    const vars = { email: email };
+    const query = `query($email: String!){
+			userHabits(email: $email) {
 				id
 				title
 				isGood
 				count
 				increments
+				isDone
 			}
-		}`;
-		const data = await graphQLFetch(query, vars);
-		return data;
-	}
+		} `;
+    const data = await graphQLFetch(query, vars);
+    return data;
+  }
 
-	constructor() {
-		super();
-		this.state = {
-			habitsList: [],
-		};
-	}
+  constructor() {
+    super();
+    this.state = {
+      habitsList: [],
+    };
+  }
 
-	async componentDidMount() {
-		const data = await this.fetchData(this.props.auth0.user.email);
-		if (data.userHabits) {
-			this.setState({
-				habitsList: data.userHabits,
-			});
-		}
-	}
+  async componentDidMount() {
+    const data = await this.fetchData(this.props.auth0.user.email);
+    if (data.userHabits) {
+      this.setState({
+        habitsList: data.userHabits,
+      });
+    }
+  }
 
-	async componentDidUpdate(prevProps) {
-		if (prevProps !== this.props) {
-			const data = await this.fetchData(this.props.auth0.user.email);
-			if (data.userHabits) {
-				this.setState({
-					habitsList: data.userHabits,
-				});
-			}
-		}
-	}
+  async componentDidUpdate(prevProps) {
+    if (prevProps !== this.props) {
+      const data = await this.fetchData(this.props.auth0.user.email);
+      if (data.userHabits) {
+        this.setState({
+          habitsList: data.userHabits,
+        });
+      }
+    }
+  }
 
-	render() {
-		return (
-			<div>
-				<AddHabit />
-				<Container fluid>
-					{this.state.habitsList ?
-						this.state.habitsList.map(habit => (
-							<Habit
-								key={habit.id}
-								title={habit.title}
-								created={JSON.stringify(habit.created)}
-								count={habit.count}
-								increments={habit.increments}
-							/>)) :
-						// render empty
-						<div></div>}
-				</Container>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <AddHabit />
+        <Container fluid>
+          {this.state.habitsList ? (
+            this.state.habitsList.map((habit) => (
+              <Habit
+                key={habit.id}
+                title={habit.title}
+                created={JSON.stringify(habit.created)}
+                count={habit.count}
+                increments={habit.increments}
+              />
+            ))
+          ) : (
+            // render empty
+            <div></div>
+          )}
+        </Container>
+      </div>
+    );
+  }
 }
 
-export default withAuth0(withAuthenticationRequired(HabitList, {
-	onRedirecting: () => (<Alert variant='info'>Redirecting...</Alert>)
-}));
+export default withAuth0(
+  withAuthenticationRequired(HabitList, {
+    onRedirecting: () => <Alert variant="info">Redirecting...</Alert>,
+  })
+);
