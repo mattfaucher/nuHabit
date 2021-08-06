0552d86b707673f0299f9870ff65352d68cd2e8b
Noam <gal.n@husky.neu.edu>
2021-08-06 15:40:55 -0400

    route badge

--- ui/src/routes.js
+++ ui/src/routes.js
@@ -3,10 +3,12 @@ import LandingPage from './LandingPage.jsx';
 import HabitList from './HabitList.jsx';
 import NotFound from './NotFound.jsx';
 import AccomplishedHabits from './AccomplishedHabits.jsx';
+import BadgeCollectionList from './badgeCollectionList.jsx';
 
 const routes = [
   { path: '/home', component: LandingPage },
   { path: '/completed', component: AccomplishedHabits },
+  { path: '/collection', component: BadgeCollectionList},
   { path: '/habits', component: HabitList },
   { path: '/details/:id,:title,:count,:increments', component: HabitDetails },
   { path: '*', component: NotFound },
